140346a201792e94ccc09e99f728d152c70b32bd
Andrew Ren <azr70@ymcasf.org>
2024-06-07 12:45:06 -0700

    changed some content in about page to make it accurate to new figma design, testing some more stuff with scraping with fever site

--- react-app/src/scrape_test.py
+++ react-app/src/scrape_test.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-page_to_scrape = requests.get("https://liftingcast.com/")
+page_to_scrape = requests.get("https://feverup.com/en/san-francisco/candlelight")
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-event_names = soup.findAll("div", attrs={"class":"table-title"})
+event_names = soup.findAll("h3", attrs={"class":"fv-plan-card-v2__title"})
+prices = soup.findAll("span", attrs={"class":"fv-plan-card-v2__price-info"})
 
-for name in event_names:
-    print(name.text)
+for name, price in zip(event_names, prices):
+    print(name.text + ": " + price.text)
